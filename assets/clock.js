8f6e205f4d946d2ed4f81dd059e1c253910654e2
Martijn Rondeel <martijn@rondeel.email>
2018-03-09 18:17:19 +0100

    Add colored text

--- assets/clock.js
+++ assets/clock.js
@@ -12,13 +12,19 @@ function zeroPadNumber(number) {
 
   // Set the background color
   const date = new Date();
-  const color = cachedBackgroundElement.style.backgroundColor = '#'
+  const hexColor = cachedBackgroundElement.style.backgroundColor = '#'
     + zeroPadNumber(date.getHours())
     + zeroPadNumber(date.getMinutes())
     + zeroPadNumber(date.getSeconds());
 
-  // Set the hex number
-  cachedTextElement.textContent = color;
+  // Set the hex number text
+  cachedTextElement.textContent = hexColor;
+
+  // Set the hex number color based on inverted background-color
+  cachedTextElement.style.color = 'rgb('
+    + (0xFF - parseInt(hexColor.substring(1, 3), 16)) + ', ' // Red
+    + (0xFF - parseInt(hexColor.substring(3, 5), 16)) + ', ' // Green
+    + (0xFF - parseInt(hexColor.substring(5, 7), 16)) + ')'; // Blue
 
   setTimeout(setColor, 1000);
 })();
