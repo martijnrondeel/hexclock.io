120bdb395459efc2590a3a45cc3c86e704595c52
Martijn Rondeel <martijn@rondeel.email>
2018-04-19 10:53:27 +0200

    Improve readability

--- assets/clock.js
+++ assets/clock.js
@@ -7,11 +7,6 @@ function zeroPadNumber(number) {
   return ('0' + number).slice(-2);
 }
 
-// Invert and rgb-ify substring of a hexadecimal number
-function hexToInvertedRgb(hexColor, start, end) {
-  return 0xFF - parseInt(hexColor.substring(start, end), 16);
-}
-
 // Set color according to time
 (function setColor() {
 
@@ -24,12 +19,6 @@ function hexToInvertedRgb(hexColor, start, end) {
 
   // Set the hex number text
   cachedTextElement.textContent = hexColor;
-
-  // Set the hex number color based on inverted background-color
-  cachedTextElement.style.color = 'rgb('
-    + (hexToInvertedRgb(hexColor, 1, 3)) + ', ' // Red
-    + (hexToInvertedRgb(hexColor, 3, 5)) + ', ' // Green
-    + (hexToInvertedRgb(hexColor, 5, 7)) + ')'; // Blue
 
   setTimeout(setColor, 1000);
 })();
